4f6e8bf2eed78676757c044e01ac41ebdb665fd1
Purushoth2209 <purushothamanramalingam22@gmail.com>
2024-11-27 00:57:23 +0530

    Commited2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sih_ui/',  // Ensure the base URL matches the repository name
   plugins: [react()],
-})
+});
